d430e5f19dda552d107bc6758af35f554da1661f
Bruno Rafael <rbrunog21@gmail.com>
2025-08-10 02:07:08 -0300

    feat: add register order use case

---
+++ api/src/OrderManagement.Application/UseCases/Order/Register/IRegisterOrderUseCase.cs
@@ -0,0 +1,10 @@
+﻿using OrderManagement.Communication.Requests;
+using OrderManagement.Communication.Responses;
+
+namespace OrderManagement.Application.UseCases.Order.Register
+{
+    public interface IRegisterOrderUseCase
+    {
+        Task<ResponseOrderJson> Execute(RequestOrderJson request);
+    }
+}
---
+++ api/src/OrderManagement.Application/UseCases/Order/Register/RegisterOrderUseCase.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using OrderManagement.Communication.Requests;
+using OrderManagement.Communication.Responses;
+using OrderManagement.Domain.Repositories;
+using OrderManagement.Domain.Services;
+using OrderManagement.Exception;
+
+namespace OrderManagement.Application.UseCases.Order.Register
+{
+    internal class RegisterOrderUseCase : IRegisterOrderUseCase
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly IUpdateOrderStatusQueue _queue;
+        public RegisterOrderUseCase(
+            IUnitOfWork unitOfWork, 
+            IOrderRepository orderRepository, 
+            IMapper mapper, 
+            IUpdateOrderStatusQueue queue)
+        {
+            _unitOfWork = unitOfWork;
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _queue = queue;
+        }
+        public async Task<ResponseOrderJson> Execute(RequestOrderJson request)
+        {
+            Validate(request);
+
+            var entity = _mapper.Map<Domain.Entities.Order>(request);
+
+            var order = await _orderRepository.Add(entity);
+
+            await _unitOfWork.Commit();
+
+            await _queue.SendMessage(order);
+
+            var orderResponse = _mapper.Map<ResponseOrderJson>(order);
+
+            return orderResponse;
+        }
+
+        private void Validate(RequestOrderJson request)
+        {
+            var validator = new OrderValidator();
+
+            var result = validator.Validate(request);
+
+            if(!result.IsValid)
+            {
+                List<string> errors = result.Errors.Select(error => error.ErrorMessage)
+                    .ToList();
+                throw new ErrorOnValidationException(errors);
+            }
+        }
+    }
+}
