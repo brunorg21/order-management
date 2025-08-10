60127f051b555430c021b78bda557e36814fac06
Bruno Rafael <rbrunog21@gmail.com>
2025-08-10 02:10:29 -0300

    feat: add validator to validate order requests

---
+++ api/src/OrderManagement.Application/UseCases/Order/OrderValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using OrderManagement.Communication.Requests;
+
+namespace OrderManagement.Application.UseCases.Order
+{
+    public class OrderValidator : AbstractValidator<RequestOrderJson>
+    {
+        public OrderValidator()
+        {
+            RuleFor(x => x.Value).GreaterThan(0).WithMessage("O valor do pedido deve ser maior que zero.");
+        }
+    }
+}
