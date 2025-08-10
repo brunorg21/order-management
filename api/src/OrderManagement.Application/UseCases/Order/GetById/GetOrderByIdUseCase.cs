5a2e815a0bacbd590a206ef4c6f4afdae2d6f2cf
Bruno Rafael <rbrunog21@gmail.com>
2025-08-10 02:06:54 -0300

    feat: add get order by id

---
+++ api/src/OrderManagement.Application/UseCases/Order/GetById/GetOrderByIdUseCase.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using OrderManagement.Communication.Responses;
+using OrderManagement.Domain.Repositories;
+using OrderManagement.Exception;
+
+namespace OrderManagement.Application.UseCases.Order.GetById
+{
+    internal class GetOrderByIdUseCase : IGetOrderByIdUseCase
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        public GetOrderByIdUseCase(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+        public async Task<ResponseOrderJson> Execute(Guid id)
+        {
+            var order = await _orderRepository.GetById(id);
+
+            if(order is null)
+            {
+                throw new NotFoundException("Pedido não encontrado.");
+            }
+
+            var orderResponse = _mapper.Map<ResponseOrderJson>(order);
+
+            return orderResponse;
+        }
+    }
+}
---
+++ api/src/OrderManagement.Application/UseCases/Order/GetById/IGetOrderByIdUseCase.cs
@@ -0,0 +1,9 @@
+﻿using OrderManagement.Communication.Responses;
+
+namespace OrderManagement.Application.UseCases.Order.GetById
+{
+    public interface IGetOrderByIdUseCase
+    {
+        Task<ResponseOrderJson> Execute(Guid id);
+    }
+}
