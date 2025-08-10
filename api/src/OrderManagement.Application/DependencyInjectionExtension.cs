c3c156bb4ba94f1dc867bcaf71fce7c9e5912a55
Bruno Rafael <rbrunog21@gmail.com>
2025-08-10 02:12:07 -0300

    feat: add use cases on dependency injection

---
+++ api/src/OrderManagement.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrderManagement.Application.AutoMapper;
+using OrderManagement.Application.UseCases.Order.Delete;
+using OrderManagement.Application.UseCases.Order.GetAll;
+using OrderManagement.Application.UseCases.Order.GetById;
+using OrderManagement.Application.UseCases.Order.Register;
+using OrderManagement.Application.UseCases.Order.Update;
+using OrderManagement.Application.UseCases.Order.Update.UpdateOrderStatus;
+
+namespace OrderManagement.Application
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            AddUseCases(services);
+            AddAutoMapper(services);
+        }
+
+        public static void AddUseCases(IServiceCollection services)
+        {
+            services.AddScoped<IRegisterOrderUseCase, RegisterOrderUseCase>();
+            services.AddScoped<IGetOrderByIdUseCase, GetOrderByIdUseCase>();
+            services.AddScoped<IGetAllOrdersUseCase, GetAllOrdersUseCase>();
+            services.AddScoped<IUpdateOrderUseCase, UpdateOrderUseCase>();
+            services.AddScoped<IDeleteOrderUseCase, DeleteOrderUseCase>();
+            services.AddScoped<IUpdateOrderStatusUseCase, UpdateOrderStatusUseCase>();
+        }
+        public static void AddAutoMapper(IServiceCollection services)
+        {
+            services.AddAutoMapper(config =>
+            {
+                config.AddProfile<AutoMapping>();
+            });
+        }
+    }
+}
