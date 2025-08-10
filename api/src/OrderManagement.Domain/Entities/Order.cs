f031d70e21d8e901b7ee052d00d5b58e68ef548d
Bruno Rafael <rbrunog21@gmail.com>
2025-08-10 02:13:20 -0300

    feat: add order entity

---
+++ api/src/OrderManagement.Domain/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using OrderManagement.Domain.Enums;
+
+namespace OrderManagement.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Customer { get; set; } = string.Empty;
+        public string Product { get; set; } = string.Empty;
+        public decimal Value { get; set; }
+        public OrderStatusType OrderStatus { get; set; } = OrderStatusType.PENDING;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ api/src/OrderManagement.Domain/Extensions/OrderStatusTypeExtension.cs
@@ -0,0 +1,18 @@
+﻿using OrderManagement.Domain.Enums;
+
+namespace OrderManagement.Domain.Extensions
+{
+    public static class OrderStatusTypeExtension
+    {
+        public static string OrderStatusToString(this OrderStatusType status)
+        {
+            return status switch
+            {
+                OrderStatusType.PENDING => "Pendente",
+                OrderStatusType.PROCESSING => "Processando",
+                OrderStatusType.COMPLETED => "Finalizado",
+                _ => string.Empty
+            };
+        }
+    }
+}
