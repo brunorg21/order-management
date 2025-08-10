bbc282de9cc6efdd1d64389648586aa564012be0
Bruno Rafael <rbrunog21@gmail.com>
2025-08-10 02:06:41 -0300

    feat: add get all orders use case

---
+++ api/src/OrderManagement.Application/UseCases/Order/GetAll/GetAllOrdersUseCase.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using OrderManagement.Communication.Responses;
+using OrderManagement.Domain.Repositories;
+
+namespace OrderManagement.Application.UseCases.Order.GetAll
+{
+    public class GetAllOrdersUseCase : IGetAllOrdersUseCase
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        public GetAllOrdersUseCase(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+        public async Task<ResponseOrderListJson> Execute()
+        {
+            var orders = await _orderRepository.GetAll();
+
+            return new ResponseOrderListJson { Orders = _mapper.Map<List<ResponseOrderJson>>(orders) };
+
+        }
+    }
+}
---
+++ api/src/OrderManagement.Application/UseCases/Order/GetAll/IGetAllOrdersUseCase.cs
@@ -0,0 +1,9 @@
+﻿using OrderManagement.Communication.Responses;
+
+namespace OrderManagement.Application.UseCases.Order.GetAll
+{
+    public interface IGetAllOrdersUseCase
+    {
+        Task<ResponseOrderListJson> Execute();
+    }
+}
