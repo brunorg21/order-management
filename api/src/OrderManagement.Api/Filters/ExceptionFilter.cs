1156dd7d540302700298bbb9f85f31119ccb9944
Bruno Rafael <rbrunog21@gmail.com>
2025-08-10 02:09:21 -0300

    feat: add error handler

---
+++ api/src/OrderManagement.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using OrderManagement.Communication.Responses;
+using OrderManagement.Exception;
+
+namespace OrderManagement.Api.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is OrderManagementException)
+            {
+                HandleException(context);
+            }
+            else
+            {
+                ThrowUnknowError(context);
+            }
+        }
+
+        private void HandleException(ExceptionContext context)
+        {
+
+            var orderManagementException = context.Exception as OrderManagementException;
+
+            var errorResponse = new ResponseErrorJson(orderManagementException.GetErrors());
+
+            context.HttpContext.Response.StatusCode = orderManagementException!.StatusCode;
+            context.Result = new ObjectResult(errorResponse);
+        }
+
+        private void ThrowUnknowError(ExceptionContext context)
+        {
+            var errorResponse = new ResponseErrorJson("Erro desconhecido.");
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Result = new ObjectResult(errorResponse);
+        }
+    }
+}
