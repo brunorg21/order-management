7cc07755ca0bea86bb24cb169a14a42a92bc4958
Bruno Rafael <rbrunog21@gmail.com>
2025-08-10 02:12:30 -0300

    feat: add order management db context

---
+++ api/src/OrderManagement.Infra/DataAccess/OrderManagementDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagement.Domain.Entities;
+
+namespace OrderManagement.Infra.DataAccess
+{
+    public class OrderManagementDbContext : DbContext
+    {
+        public OrderManagementDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
