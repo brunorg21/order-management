b0a022160132b525aa0c7eaf144f43964c6b6cb6
Bruno Rafael <rbrunog21@gmail.com>
2025-08-10 02:07:41 -0300

    feat: add auto mapping

---
+++ api/src/OrderManagement.Application/AutoMapper/AutoMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using OrderManagement.Communication.Requests;
+using OrderManagement.Communication.Responses;
+using OrderManagement.Domain.Entities;
+using OrderManagement.Domain.Extensions;
+
+namespace OrderManagement.Application.AutoMapper
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            CreateMap<RequestOrderJson, Order>();
+            CreateMap<Order, ResponseOrderJson>()
+                .ForMember(dest => dest.OrderStatus, options => options.MapFrom(src => src.OrderStatus.OrderStatusToString()));
+        }
+    }
+}
