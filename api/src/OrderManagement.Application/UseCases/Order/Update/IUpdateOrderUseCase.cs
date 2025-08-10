437028992e35821d06c2c06744611e7f0aefd000
Bruno Rafael <rbrunog21@gmail.com>
2025-08-10 02:07:25 -0300

    feat: add update order use case

---
+++ api/src/OrderManagement.Application/UseCases/Order/Update/IUpdateOrderUseCase.cs
@@ -0,0 +1,10 @@
+﻿using OrderManagement.Communication.Requests;
+using OrderManagement.Communication.Responses;
+
+namespace OrderManagement.Application.UseCases.Order.Update
+{
+    public interface IUpdateOrderUseCase
+    {
+        Task<ResponseOrderJson> Execute(RequestOrderJson request, Guid id);
+    }
+}
---
+++ api/src/OrderManagement.Application/UseCases/Order/Update/UpdateOrderUseCase.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using OrderManagement.Communication.Requests;
+using OrderManagement.Communication.Responses;
+using OrderManagement.Domain.Repositories;
+using OrderManagement.Exception;
+
+namespace OrderManagement.Application.UseCases.Order.Update
+{
+    internal class UpdateOrderUseCase : IUpdateOrderUseCase
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public UpdateOrderUseCase(IUnitOfWork unitOfWork, IOrderRepository orderRepository, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+        public async Task<ResponseOrderJson> Execute(RequestOrderJson request, Guid id)
+        {
+            Validate(request);
+
+            var order = await _orderRepository.GetById(id, false);
+
+            if(order is null)
+            {
+                throw new NotFoundException("Pedido não encontrado.");
+            }
+
+            order.Value = request.Value;
+            order.Customer = request.Customer;
+            order.Product = request.Product;
+
+             _orderRepository.Update(order);
+
+            await _unitOfWork.Commit();
+
+            return _mapper.Map<ResponseOrderJson>(order);
+        }
+        private void Validate(RequestOrderJson request)
+        {
+            var validator = new OrderValidator();
+
+            var result = validator.Validate(request);
+
+            if(!result.IsValid)
+            {
+                var errors = result.Errors.Select(error => error.ErrorMessage)
+                    .ToList();
+
+                throw new ErrorOnValidationException(errors);
+            }
+        }
+    }
+}
